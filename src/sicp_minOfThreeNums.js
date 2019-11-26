c248c73b004b0c1c7f1093cdc807bdbc13b583e3
alex-ismailov <alexander.is.dev@gmail.com>
2019-11-26 12:48:21 +0300

    modified: src/sicp_minOfThreeNums.js

--- src/sicp_minOfThreeNums.js
+++ src/sicp_minOfThreeNums.js
@@ -9,7 +9,19 @@ const minOfThreeNums = (a, b, c) => {
     return minNum;
 };
 
-[
+const minOfThreeNums2 = (a, b, c) => {
+    if (a <= b && a <= c) {
+        return a;
+    }
+    if (b <= a && b <= c) {
+        return b;
+    }
+    if (c <= a && c <= b) {
+        return c;
+    }
+};
+
+const array = [
     [1, 2, 3],
     [1, 3, 2],
     [2, 1, 3],
@@ -18,6 +30,14 @@ const minOfThreeNums = (a, b, c) => {
     [199, 200, 53],
     [53, 54, 104],
     [25, 25, 27],
-].forEach((el) => {
+    [3, 3, 4],
+];
+console.log('*** minOfThreeNums testintg ***');
+array.forEach((el) => {
     console.log(`minOfThreeNums(${el[0]} - ${el[1]} - ${el[2]}) is ${minOfThreeNums(el[0], el[1], el[2])}`);
+});
+
+console.log('*** minOfThreeNums2 testintg ***');
+array.forEach((el) => {
+    console.log(`minOfThreeNums2(${el[0]} - ${el[1]} - ${el[2]}) is ${minOfThreeNums2(el[0], el[1], el[2])}`);
 });
