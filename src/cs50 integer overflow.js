43b61c6266a822ad498830d2eae3a5d781f4bdf2
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-11 00:18:08 +0300

    added: cs50 integer overflow.js

---
+++ src/cs50 integer overflow.js
@@ -0,0 +1,10 @@
+const func = () => {
+  for (let i = new Number(9007199254740991); true; i += 1) {
+    console.log(i);
+  }
+};
+
+// func();
+// console.log(Number.MAX_VALUE);
+console.log(0 / 0); // NaN
+console.log(2 / 0); // Infinity
