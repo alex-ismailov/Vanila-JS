1c2e81928d43ed8766005306063c86fec71acb7e
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-15 01:04:23 +0300

    added: new comments

--- src/quick sort.js
+++ src/quick sort.js
@@ -5,20 +5,20 @@ const swap = (items, firstIndex, secondIndex) => {
 };
 
 const partition = (items, left, right) => {
-  const middle = items[Math.floor((left + right) / 2)]; // в качестве разделителя берем элемент из середины
+  const pivot = items[Math.floor((left + right) / 2)]; // в качестве разделителя берем элемент из середины
   let leftPointer = left;
   let rightPointer = right;
-  while (leftPointer <= rightPointer) { // пока левый указатель меньше или равен правому
-    while (items[leftPointer] < middle) {
-      leftPointer += 1; // сдвигаем левый указатель к центру если очередной элемент меньше среднего элемента
+  while (leftPointer <= rightPointer) { // пока левый указатель меньше или равен правому, т.е. пока они не пересекли опорный элемент
+    while (items[leftPointer] < pivot) { // если items[leftPointer] < pivot, то это значит, что элемент на месте 
+      leftPointer += 1; // и мы сдвигаем левый указатель направо к опорному элементу (к pivot(у))  
     }
-    while (items[rightPointer] > middle) {
-      rightPointer -= 1; // сдвигаем правый указатель к центру если очередной элемент меньше среднего элемента
+    while (items[rightPointer] > pivot) { // если items[rightPointer] < pivot, то это значит, что элемент на месте
+      rightPointer -= 1; // сдвигаем правый указатель к центру если очередной элемент меньше pivot элемента
     }
-    if (leftPointer <= rightPointer) { // если в результате сдвигов левый указатель меньше или равен правому
-      swap(items, leftPointer, rightPointer);
-      leftPointer += 1;
-      rightPointer -= 1;
+    if (leftPointer <= rightPointer) { // если в результате сдвигов левый указатель меньше или равен правому, значит еще есть не отсортированные элементы
+      swap(items, leftPointer, rightPointer); // переносим меньший элемент налево, а больший направо
+      leftPointer += 1; // двигаем указатели в поисках новых не отсортированных элементов
+      rightPointer -= 1; // каждый (leftPointer и rightPointer) в своем изначальном направлении
     }
   }
   return leftPointer;
@@ -26,13 +26,13 @@ const partition = (items, left, right) => {
 
 const quickSort = (items, left = 0, right = items.length - 1) => {
   let index;
-  if (items.length > 1) { // если длина массива больше 1
-    index = partition(items, left, right);
+  if (items.length > 1) { // если длина массива больше 1, это значит, что вероятно есть еще не отсортированные элементы
+    index = partition(items, left, right); // получаем разделитель массива 
     if (left < index - 1) {
-      quickSort(items, left, index - 1); // запускаем quickSort для левой части
+      quickSort(items, left, index - 1); // рекурсивно запускаем quickSort для левой части
     }
     if (index < right) {
-      quickSort(items, index, right); // запускаем quickSort для правой части
+      quickSort(items, index, right); // рекурсивно запускаем quickSort для правой части
     }
   }
   return items;
