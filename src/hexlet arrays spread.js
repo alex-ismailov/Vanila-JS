1795290e13be47a103f306bc0f388fe78cffcf35
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-16 16:20:17 +0300

    added: new test

--- src/hexlet arrays spread.js
+++ src/hexlet arrays spread.js
@@ -4,5 +4,10 @@ const newArr1 = [...arr1, 4, 5, 6];
 const arr2 = [4, 5, 6];
 const newArr2 = [...arr1, ...arr2];
 
+const arr3 = newArr2;
+const arr4 = [10, 11, 12];
+const newArr3 = [...arr3, 7, 8, 9, ...arr4];
+
 console.log(newArr1);
 console.log(newArr2);
+console.log(newArr3);
