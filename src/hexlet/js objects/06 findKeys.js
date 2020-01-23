2248281e02085781563775c51cb84b183a9f30da
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-23 12:56:54 +0300

    added: findKeys function

---
+++ src/hexlet/js objects/06 findKeys.js
@@ -0,0 +1,27 @@
+/* Рассмотрим пример. Реализуем функцию findKeys,
+которая возвращаетсписок ключей объекта,
+значение которых равно переданному значению: */
+
+const lessonMembers = {
+  syntax: 3,
+  using: 2,
+  foreach: 10,
+  operations: 10,
+  destructuring: 2,
+  array: 2,
+};
+
+const findKeys = (obj, expectedValue) => {
+  const result = [];
+  const entries = Object.entries(obj);
+  for (const [key, value] of entries) {
+    if (value === expectedValue) {
+      result.push(key);
+    }
+  }
+  return result;
+};
+
+console.log(findKeys(lessonMembers, 10)); // ['foreach', 'operations']
+console.log(findKeys(lessonMembers, 3));  // ['syntax']
+console.log(findKeys(lessonMembers, 2));
