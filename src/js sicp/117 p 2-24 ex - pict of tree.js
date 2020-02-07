a8cff8332f96f94f3e26fad9072800ae345584aa
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-07 11:12:45 +0300

    fixed: eslint warns

--- src/js sicp/117 p 2-24 ex - pict of tree.js
+++ src/js sicp/117 p 2-24 ex - pict of tree.js
@@ -3,10 +3,12 @@
 результат напечатает интерпретатор, изобразите его в виде стрелочной диаграммы, а
 также его интерпретацию в виде дерева (как на рисунке 2.6). */
 
-import { l, isEmpty, head, tail, cons, concat, filter, isList, toString as listToString } from '@hexlet/pairs-data';
+import {
+  l, toString as listToString,
+} from '@hexlet/pairs-data';
 
 console.log(listToString(l(1, l(2, l(3, 4))))); // (1, (2, (3, 4)))
-/* 
+/*
 (1, (2, (3, 4)))
         /\
        /  \
