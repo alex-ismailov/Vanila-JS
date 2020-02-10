7be8c1527c3a9a5744eb383fd0ae90e1c05192ff
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-10 23:46:48 +0300

    added: sumOddSquares function

---
+++ src/js sicp/02 build abstr-s with data/120 - sumOddSquares.js
@@ -0,0 +1,21 @@
+import {
+  l, isEmpty, head, tail, toString as listToString, isList,
+} from '@hexlet/pairs-data';
+
+const isOdd = (n) => n % 2 !== 0;
+const square = (n) => n ** 2;
+
+const sumOddSquares = (tree) => {
+  if (isEmpty(tree)) {
+    return 0;
+  }
+  if (isList(head(tree))) {
+    return sumOddSquares(head(tree)) + sumOddSquares(tail(tree));
+  }
+  const res = isOdd(head(tree)) ? square(head(tree)) : 0;
+  return res + sumOddSquares(tail(tree));
+};
+
+/* testing */
+const data = l(1, 2, l(3, l(4, 5)));
+console.log(listToString(sumOddSquares(data)));
