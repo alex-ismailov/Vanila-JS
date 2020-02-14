88c7b5db1025321077dd168a4b63ca20abe55cbe
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-14 23:36:39 +0300

    added: permutations function

---
+++ src/js sicp/02 build abstr-s with data/130 - permutations.js
@@ -0,0 +1,24 @@
+import { l, cons, isEmpty, toString as listToString } from "@hexlet/pairs-data";
+import {
+  foldRight, lMap, enumerateInterval, lFilter, flatMap,
+} from '../../myLib/seqlib';
+
+/* Вложенные отображения полезны не только для таких последовательностей, которые
+перечисляют интервалы (как в 128 - makeSeqOfOrderedPairs). Допустим, нам нужно
+перечислить все перестановки множества S, то есть все способы упорядочить это множество.
+Например, перестановки множества {1, 2, 3} — это :
+{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2} и {3, 2, 1}.
+*/
+
+const remove = (elem, set) => lFilter((curr) => curr !== elem, set);
+
+const permutations = (s) => {
+  if (isEmpty(s)) {
+    return l(l());
+  }
+  return flatMap((x) => lMap((p) => cons(x, p), permutations(remove(x, s))), s);
+};
+
+/* testing */
+const set = l(1, 2, 3);
+console.log(listToString(permutations(set)));
