364a438dce34ff9ccb58b72a8ecc6c3205afde66
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-16 21:45:41 +0300

    refactored

--- src/js sicp/02 build abstr-s with data/130 - ex 2.41 - triples.js
+++ src/js sicp/02 build abstr-s with data/130 - ex 2.41 - triples.js
@@ -3,12 +3,6 @@ import { l, head, tail, concat, toString as listToString, cons, isEmpty } from '
 
 const uniquePairs = (n) => flatMap((i) => lMap((j) => l(i, j), enumerateInterval(1, i - 1)), enumerateInterval(1, n));
 
-// const makeOrderedPairs = (n) => foldRight(
-//   concat, l(), lMap((i) => lMap((j) => l(i, j), enumerateInterval(1, i - 1)), enumerateInterval(1, n))
-// );
-
-// const uniqueTriples = (n) => flatMap((i) => flatMap((j) => lMap((k) => l(i, j, k), enumerateInterval(1, j - 1)), enumerateInterval(1, i - 1)), enumerateInterval(1, n));
-
 const uniqueTriples = (n) => (
   flatMap(
     (i) => flatMap(
