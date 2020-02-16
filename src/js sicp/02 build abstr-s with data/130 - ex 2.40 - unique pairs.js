893cf7c8081df78c210af9292ef6f9904ec16836
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-16 21:43:43 +0300

    refactored

--- src/js sicp/02 build abstr-s with data/130 - ex 2.40 - unique pairs.js
+++ src/js sicp/02 build abstr-s with data/130 - ex 2.40 - unique pairs.js
@@ -1,44 +1,39 @@
-import { flatMap, lMap, enumerateInterval, lFilter } from '../../myLib/seqlib';
-import { l, head, tail, toString as listToString, cons } from '@hexlet/pairs-data';
+import {
+  l, head, tail, toString as listToString,
+} from '@hexlet/pairs-data';
+import {
+  flatMap, lMap, enumerateInterval, lFilter,
+} from '../../myLib/seqlib';
 
 const isPrime = (n) => {
   const iter = (testDiv) => {
     if (testDiv > Math.floor(Math.sqrt(n))) {
       return true;
     }
-    if (n % testDiv === 0) {
-      return false;
-    }
-    return iter(testDiv + 1);
+    return n % testDiv === 0
+      ? false
+      : iter(testDiv + 1);
   };
   return iter(2);
 };
 
-const isSumPrime = (pair) => {
-  return isPrime(head(pair) + head(tail(pair)));
-};
+const isSumPrime = (pair) => isPrime(head(pair) + head(tail(pair)));
 
-const uniquePairs = (n) => {
-  const fm = flatMap((i) => lMap((j) => l(i, j), enumerateInterval(1, i - 1)), enumerateInterval(1, n));
-  return fm;
-};
+const uniquePairs = (n) => (
+  flatMap((i) => lMap((j) => l(i, j),
+    enumerateInterval(1, i - 1)),
+  enumerateInterval(1, n))
+);
 
-const showPrimePairs = (pair) => {
-  return l(head(pair), head(tail(pair)), head(pair) + head(tail(pair)));
-};
-
-// const primeSumPairs = (n) => {
-//   const pairs = uniquePairs(n);
-//   const filtered = lFilter(isSumPrime, pairs);
-//   return lMap(showPrimePairs, filtered);
-// };
+const showPrimePairs = (pair) => (
+  l(head(pair), head(tail(pair)), head(pair) + head(tail(pair)))
+);
 
 const primeSumPairs = (n) => (
-  lMap(showPrimePairs, 
-    lFilter(isSumPrime, 
+  lMap(showPrimePairs,
+    lFilter(isSumPrime,
       uniquePairs(n)))
 );
-  
 
 /* testing */
 console.log(listToString(primeSumPairs(6)));
