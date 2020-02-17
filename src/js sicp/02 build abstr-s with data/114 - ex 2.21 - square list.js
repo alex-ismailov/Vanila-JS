f7c4eef800e348ec16599c4340b8e08c83857267
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 22:03:18 +0300

    added: ex 2.21 - square list function

--- src/js sicp/02 build abstr-s with data/114 - ex 2.21 - square list.js
+++ src/js sicp/02 build abstr-s with data/114 - ex 2.21 - square list.js
@@ -1,3 +1,9 @@
+import {
+  l, isEmpty, head, tail, cons, toString as listToString,
+} from '@hexlet/pairs-data';
+
+import { lMap } from '../../myLib/seqlib';
+
 /* Упражнение 2.21.
 Процедура square-list принимает в качестве аргумента список чисел и
 возвращает список квадратов этих чисел.
@@ -15,24 +21,13 @@
   (map h??i h??i))
 */
 
-import {
-  l, isEmpty, head, tail, cons, toString as listToString,
-} from '@hexlet/pairs-data';
+const squareList = (items) => (
+  isEmpty(items)
+    ? l()
+    : cons(head(items) ** 2, squareList(tail(items)))
+);
 
-import { lMap } from '../../myLib/lmap';
-
-/* version 1 */
-const squareList = (items) => {
-  if (isEmpty(items)) {
-    return l();
-  }
-  return cons(head(items) ** 2, squareList(tail(items)));
-};
-
-/* version 2 */
-const squareList2 = (items) => {
-  return lMap((n) => n ** 2, items);
-};
+const squareList2 = (items) => lMap((curr) => curr ** 2, items);
 
 /* testing */
 const list = l(1, 2, 3, 4, 5);
