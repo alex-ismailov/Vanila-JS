1c3bb634f885a1e30f3d5794db9faf2211976e87
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 14:28:25 +0300

    fixed: eslint errors

--- src/js sicp/02 build abstr-s with data/111 - ex 2.18 - reverse.js
+++ src/js sicp/02 build abstr-s with data/111 - ex 2.18 - reverse.js
@@ -4,7 +4,9 @@
 (reverse (list 1 4 9 16 25))
 (25 16 9 4 1) */
 
-import { l, isEmpty, head, tail, cons, concat, filter, toString as listToString } from '@hexlet/pairs-data';
+import {
+  l, isEmpty, head, tail, cons, toString as listToString,
+} from '@hexlet/pairs-data';
 
 const reverse = (list) => {
   const iter = (curr, acc) => (
@@ -16,5 +18,5 @@ const reverse = (list) => {
 };
 
 /* testing */
-const list = l(1, 4, 9, 16, 25); 
+const list = l(1, 4, 9, 16, 25);
 console.log(listToString(reverse(list)));
