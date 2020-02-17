c2f08a4c2b2aed11b4207a807faa1529c2f9e48c
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 23:47:03 +0300

    refactored

--- src/js sicp/02 build abstr-s with data/118 - ex 2.28 - fringe (using fold right).js
+++ src/js sicp/02 build abstr-s with data/118 - ex 2.28 - fringe (using fold right).js
@@ -1,16 +1,23 @@
 import {
   l, isEmpty, isList, head, tail, cons, concat, toString as listToString,
 } from '@hexlet/pairs-data';
-import { foldLeft, foldRight } from '../../myLib/higherOrderFunctions';
+import { foldLeft, foldRight } from '../../myLib/seqlib';
 
 
 /* flat tree */
-const flat = (tree) => foldRight((curr, acc) => {
-  if (isList(curr)) {
-    return concat(flat(curr), acc);
-  }
-  return cons(curr, acc);
-}, l(), tree);
+
+const flat = (tree) => foldRight((curr, acc) => (
+  isList(curr)
+    ? concat(flat(curr), acc)
+    : cons(curr, acc)
+), l(), tree);
+
+// const flat = (tree) => foldRight((curr, acc) => {
+//   if (isList(curr)) {
+//     return concat(flat(curr), acc);
+//   }
+//   return cons(curr, acc);
+// }, l(), tree);
 
 /* testing */
 const tree = l(l(1, 2, 3), l(4, l(5, l(6))), 7);
