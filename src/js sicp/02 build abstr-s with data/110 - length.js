6b8ee8df78c4c68059bb277822bb4c9c9e3ed48c
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 13:51:36 +0300

    refactored: length function

--- src/js sicp/02 build abstr-s with data/110 - length.js
+++ src/js sicp/02 build abstr-s with data/110 - length.js
@@ -1,7 +1,13 @@
-import { l, isEmpty, head, tail, cons, concat, filter, toString as listToString } from '@hexlet/pairs-data';
+import { l, isEmpty, tail } from '@hexlet/pairs-data';
+
+const length = (items) => (
+  isEmpty(items)
+    ? 0
+    : 1 + length(tail(items))
+);
 
 /* linear recursive process */
-const length = (list) => {
+const length1 = (list) => {
   if (isEmpty(list)) {
     return 0;
   }
@@ -9,30 +15,31 @@ const length = (list) => {
 };
 
 /* linear iterative process */
-const length_2 = (list) => {
-  const iter = (list, acc) => {
-    if (isEmpty(list)) {
+const length2 = (list) => {
+  const iter = (curr, acc) => {
+    if (isEmpty(curr)) {
       return acc;
     }
-    return iter(tail(list), acc + 1);
+    return iter(tail(curr), acc + 1);
   };
-  return iter(list, 0)
+  return iter(list, 0);
 };
 
 /* cicle way */
-const length_3 = (list) => {
-  let length = 0;
-  while(!isEmpty(list)) {
-    list = tail(list);
-    length += 1;
+const length3 = (list) => {
+  let len = 0;
+  let curr = list;
+  while (!isEmpty(curr)) {
+    curr = tail(curr);
+    len += 1;
   }
-  return length;
+  return len;
 };
 
 /* testing */
-// const list = l(10, 20, 30, 40, 50);
-// console.log(length(list)); // 5
-// console.log(length_2(list)); // 5
-// console.log(length_3(list)); // 5
+const list = l(10, 20, 30, 40, 50);
+console.log(length(list)); // 5
 
-export { length };
+console.log(length1(list)); // 5
+console.log(length2(list)); // 5
+console.log(length3(list)); // 5
