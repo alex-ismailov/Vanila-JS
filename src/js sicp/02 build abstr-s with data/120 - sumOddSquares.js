9ba4be61f9a7c092336836886c8e487c1df8d6dc
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 12:58:27 +0300

    refactored: sumOddSquares function

--- src/js sicp/02 build abstr-s with data/120 - sumOddSquares.js
+++ src/js sicp/02 build abstr-s with data/120 - sumOddSquares.js
@@ -5,16 +5,26 @@ import {
 const isOdd = (n) => n % 2 !== 0;
 const square = (n) => n ** 2;
 
-const sumOddSquares = (tree) => {
-  if (isEmpty(tree)) {
-    return 0;
-  }
-  if (isList(head(tree))) {
-    return sumOddSquares(head(tree)) + sumOddSquares(tail(tree));
-  }
-  const res = isOdd(head(tree)) ? square(head(tree)) : 0;
-  return res + sumOddSquares(tail(tree));
-};
+// const sumOddSquares = (tree) => {
+//   if (isEmpty(tree)) {
+//     return 0;
+//   }
+//   if (isList(head(tree))) {
+//     return sumOddSquares(head(tree)) + sumOddSquares(tail(tree));
+//   }
+//   const res = isOdd(head(tree)) ? square(head(tree)) : 0;
+//   return res + sumOddSquares(tail(tree));
+// };
+
+const sumOddSquares = (tree) => (
+  isEmpty(tree)
+    ? 0
+    : isList(head(tree))
+      ? sumOddSquares(head(tree)) + sumOddSquares(tail(tree))
+      : (isOdd(head(tree)) ? square(head(tree)) : 0)
+        +
+        sumOddSquares(tail(tree))
+);
 
 /* testing */
 const data = l(1, 2, l(3, l(4, 5)));
