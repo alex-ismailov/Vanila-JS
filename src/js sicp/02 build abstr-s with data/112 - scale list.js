88e415a500bf36baad6166a97b3f4c491ea95ba0
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 18:29:28 +0300

    added new defifnition of scaleList in terms of lMap

--- src/js sicp/02 build abstr-s with data/112 - scale list.js
+++ src/js sicp/02 build abstr-s with data/112 - scale list.js
@@ -1,6 +1,7 @@
 import {
   l, head, tail, isEmpty, cons, toString as listToString,
 } from '@hexlet/pairs-data';
+import { lMap } from '../../myLib/seqlib';
 
 const scaleList = (items, factor) => (
   isEmpty(items)
@@ -8,6 +9,10 @@ const scaleList = (items, factor) => (
     : cons(head(items) * factor, scaleList(tail(items), factor))
 );
 
+/* new defifnition of scaleList in terms of lMap */
+const scaleListMap = (items, factor) => lMap((curr) => curr * factor, items);
+
 /* testing */
 const data = l(1, 2, 3, 4, 5);
 console.log(listToString(scaleList(data, 2)));
+console.log(listToString(scaleListMap(data, 2)));
