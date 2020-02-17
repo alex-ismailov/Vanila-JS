c167940c7506b973f79cf876e0dcc53ef610a010
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-17 13:55:39 +0300

    refactored: ex 2.41 - triples

--- src/js sicp/02 build abstr-s with data/130 - ex 2.41 - triples.js
+++ src/js sicp/02 build abstr-s with data/130 - ex 2.41 - triples.js
@@ -1,22 +1,23 @@
-import { flatMap, lMap, enumerateInterval, lFilter, foldRight } from '../../myLib/seqlib';
-import { l, head, tail, concat, toString as listToString, cons, isEmpty } from '@hexlet/pairs-data';
-
-const uniquePairs = (n) => flatMap((i) => lMap((j) => l(i, j), enumerateInterval(1, i - 1)), enumerateInterval(1, n));
+import {
+  l, toString as listToString, isEmpty,
+} from '@hexlet/pairs-data';
+import {
+  flatMap, lMap, enumerateInterval, lFilter, foldRight,
+} from '../../myLib/seqlib';
 
 const uniqueTriples = (n) => (
-  flatMap(
-    (i) => flatMap(
-      (j) => lMap(
-        (k) => l(i, j, k), enumerateInterval(1, j - 1)
-      ),
-      enumerateInterval(1, i - 1)
-    ),
-    enumerateInterval(1, n))
+  flatMap((i) => flatMap(
+    (j) => lMap((k) => l(i, j, k),
+      enumerateInterval(1, j - 1)),
+    enumerateInterval(1, i - 1),
+  ),
+  enumerateInterval(1, n))
 );
 
-const triplesSum = (s, n) => {
-  return lFilter((triple) => (s === foldRight((curr, acc) => acc + curr, 0, triple)), uniqueTriples(6));
-};
+const triplesSum = (s) => (
+  lFilter((triple) => (s === foldRight((curr, acc) => acc + curr, 0, triple)),
+    uniqueTriples(6))
+);
 
 /* testing */
 for (let i = 0; i < 10000; i += 1) {
@@ -24,4 +25,17 @@ for (let i = 0; i < 10000; i += 1) {
   if (!isEmpty(res)) {
     console.log(`i = ${i}, triples: ${listToString(res)}`);
   }
-};
+}
+
+// const testing = (curr, end) => {
+//   if (curr >= end) {
+//     return ;
+//   }
+//   const res = triplesSum(curr);
+//   if (!isEmpty(res)) {
+//     console.log(`i = ${curr}, triples: ${listToString(res)}`);
+//   }
+//   return testing(curr + 1, end);
+// };
+
+// testing(1, 8000);
