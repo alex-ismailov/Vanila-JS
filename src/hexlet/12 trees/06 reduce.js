bda9660ad41b798b04513b15d7f24fd7981d01c4
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-25 11:36:55 +0300

    add(12 trees/) 06 reduce.js

---
+++ src/hexlet/12 trees/06 reduce.js
@@ -0,0 +1,20 @@
+const reduce = (fn, tree, acc) => {
+  const [, children] = tree;
+  const newAcc = fn(acc);
+
+  if (!children) {
+    return newAcc;
+  }
+
+  return children.reduce((iAcc, n) => reduce(fn, n, iAcc), newAcc);
+};
+
+/* testing */
+const tree = ['A', [
+  ['B', [['E'], ['F']]],
+  ['C'],
+  ['D', [['G'], ['J']]],
+]];
+
+const res = reduce((acc) => acc + 1, tree, 0); // 8
+console.log(res);
