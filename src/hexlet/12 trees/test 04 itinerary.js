7318e0c75780243c0b427f714e1587650b82c96e
smilenb <alexander.is.dev@gmail.com>
2020-04-01 00:35:42 +0300

    refactor(12 trees/test 04 itinerary.js) use hof map instead of for of cicle

--- src/hexlet/12 trees/test 04 itinerary.js
+++ src/hexlet/12 trees/test 04 itinerary.js
@@ -21,6 +21,69 @@ import _ from 'lodash';
 //   Rzhev: ['Tver', []],
 // };
 
+const makeAdjacencyList = (tree, dict, parent = null) => {
+  const [nodeName, branches] = tree;
+  const children = [];
+  dict[nodeName] = [parent, children];
+  if (branches) {
+    branches.map((branch) => {
+      const name = makeAdjacencyList(branch, dict, parent = nodeName);
+      children.push(name);
+    });
+  }
+  return nodeName;
+};
+
+const getCommonParent = (first, second, dict) => {
+  const [parentName] = dict[first];
+  if (parentName === null) {
+    return first;
+  }
+  const parent = dict[parentName];
+  const [, parentChildren] = parent;
+  if (parentChildren.includes(second)) {
+    return parentName;
+  }
+  return getCommonParent(parentName, second, dict);
+};
+
+const getUpRoute = (from, to, dict) => {
+  const iter = (from, to, route) => {
+    if (from === to) {
+      route.push(from);
+      return route;
+    }
+    route.push(from);
+    const [parent] = dict[from];
+    return iter(parent, to, route);
+  };
+  return iter(from, to, []);
+};
+
+const getDownRoute = (from, to, dict) => {
+  const iter = (from, to, route) => {
+    if (from === to) {
+      route.push(from);
+      return route.reverse();
+    }
+    route.push(from);
+    const [parent] = dict[from];
+    return iter(parent, to, route);
+  };
+  return iter(from, to, []);
+};
+
+const itinerary = (tree, from, to) => {
+  const adjacencyList = {};
+  makeAdjacencyList(tree, adjacencyList);
+  const commonParent = getCommonParent(from, to, adjacencyList);
+  const upRoute = getUpRoute(from, commonParent, adjacencyList);
+  const downRoute = getDownRoute(to, commonParent, adjacencyList);
+
+  return _.uniq([...upRoute, ...downRoute]);
+};
+
+/* testing */
 const tree = ['Moscow', [
   ['Smolensk'],
   ['Yaroslavl'],
@@ -43,57 +106,6 @@ const tree = ['Moscow', [
   ]],
 ]];
 
-
-const makeAdjacencyList = (tree, dict, parent = null) => {
-  const [nodeName, branches] = tree;
-  const children = [];
-  dict[nodeName] = [parent, children];
-  if (branches) {
-    for (const branch of branches) {
-      const name = makeAdjacencyList(branch, dict, parent = nodeName);
-      children.push(name);
-    }
-  }
-  return nodeName;
-};
-
-const getCommonParent = (first, second, dict) => {
-  const [parentName] = dict[first];
-  if (parentName === null) {
-    return first;
-  }
-  const parent = dict[parentName];
-  const [, parentChildren] = parent; 
-  if (parentChildren.includes(second)) {
-    return parentName;
-  }
-  return getCommonParent(parentName, second, dict);
-};
-
-const getRoute = (from, to, dict) => {
-  const iter = (from, to, route) => {
-    if (from === to) {
-      route.push(from);
-      return route;
-    }
-    route.push(from);
-    const [parent] = dict[from];
-    return iter(parent, to, route);
-  };
-  return iter(from, to, []);
-};
-
-const itinerary = (tree, from, to) => {
-  const adjacencyList = {};
-  makeAdjacencyList(tree, adjacencyList);
-  const commonParent = getCommonParent(from, to, adjacencyList);
-  const upRoute = getRoute(from, commonParent, adjacencyList);
-  const downRoute = getRoute(to, commonParent, adjacencyList);
-
-  return _.uniq([...upRoute, ...downRoute]);
-};
-
-/* testing */
 console.log(itinerary(tree, 'Dubna', 'Kostroma'));
 // ['Dubna', 'Tver', 'Moscow', 'Ivanovo', 'Kostroma']
 
