d73e969f9ecc5832f2ad21e896f307c1858fe3db
smilenb <alexander.is.dev@gmail.com>
2020-06-03 23:10:50 +0300

    add(src/hexlet/) 19 Frontend Architecture/

---
+++ src/hexlet/19 Frontend Architecture/02 Application status/test.js
@@ -0,0 +1,15 @@
+  const input = document.querySelector('.phone');
+  input.addEventListener('keyup', () => {
+    if (input.value === '') {
+      state.registrationProcess.valid = true;
+      state.registrationProcess.submitDisabled = true;
+    } else if (!input.value.match(/^\d+$/)) {
+      state.registrationProcess.valid = false;
+      state.registrationProcess.submitDisabled = true;
+    } else {
+      state.registrationProcess.valid = true;
+      state.registrationProcess.submitDisabled = false;
+    }
+    render(state);
+  });
+};
