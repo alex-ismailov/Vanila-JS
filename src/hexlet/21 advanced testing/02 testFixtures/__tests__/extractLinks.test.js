b9ab881c7a9492b58f8c380bfa03ea1f0ba053e3
smilenb <alexander.is.dev@gmail.com>
2020-06-18 00:21:41 +0300

    fixed(src/hexlet/21 advanced testing/02 testFixtures/__tests__/extractLinks.test.js) import fs

--- src/hexlet/21 advanced testing/02 testFixtures/__tests__/extractLinks.test.js
+++ src/hexlet/21 advanced testing/02 testFixtures/__tests__/extractLinks.test.js
@@ -1,21 +1,11 @@
-import fs from 'fs';
+// import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 
 import extractLinks from '../extractLinks';
 
-test('extractLinks', async () => {
-  const html = await fs.readFile(`${__dirname}__fixtures__/index.html`, 'utf-8');
-  const links = extractLinks(html);
-  expect(links).toEqual(['/resumes/1', '/users/6']);
-});
-
 // test('extractLinks', async () => {
-//   // HTML находится в файле withLinks.html в директории __fixtures__ 
-//   // При чтении текстовых файлов, в конце может добавляться пустая строка.
-//   // Она удаляется с помощью метода `trim`, если нужно
-//   // __dirname – директория, в которой находится данный файл с тестами
-//   const html = await fs.readFile(`${__dirname}/../__fixtures__/withLinks.html`, 'utf-8');
-//   // Теперь с HTML удобно работать и он не загромождает тесты.
+//   const html = await fs.readFile(`${__dirname}/__fixtures__/index.html`, 'utf-8');
 //   const links = extractLinks(html);
 //   expect(links).toEqual(['/resumes/1', '/users/6']);
 // });
@@ -23,15 +13,15 @@ test('extractLinks', async () => {
 /* Когда фикстур больше одной, то в коде тестов начинает появляться много похожих вызовов, считывающих файлы,
 В таком случае лучше вынести построение пути в отдельную функцию, а заодно воспользоваться правильным способом
 склеивания путей: */
-// test('extractLinks', async () => {
-//   const getFixturePath = (filename) => path.join(__dirname, '/__fixtures__/', filename);
-//   const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-//   const html = await readFile('index.html');
-//   const html2 = await readFile('someAnother.html');
+test('extractLinks', async () => {
+  const getFixturePath = (filename) => path.join(__dirname, '/__fixtures__/', filename);
+  const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+  const html = await readFile('index.html');
+  const html2 = await readFile('someAnother.html');
 
-//   const links = extractLinks(html);
-//   expect(links).toEqual(['/resumes/1', '/users/6']);
+  const links = extractLinks(html);
+  expect(links).toEqual(['/resumes/1', '/users/6']);
 
-//   const links2 = extractLinks(html2);
-//   expect(links2).toEqual(['/link1', '/link2']);
-// });
+  const links2 = extractLinks(html2);
+  expect(links2).toEqual(['/link1', '/link2']);
+});
