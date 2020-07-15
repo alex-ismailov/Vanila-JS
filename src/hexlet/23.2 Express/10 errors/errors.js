5595a5c5b093385d1c0e6ae4ee5d83f26f8661c2
smlnx <alexander.is.dev@gmail.com>
2020-07-15 14:54:24 +0300

    add(src/hexlet/23.2 Express/10 errors/errors.js) more examples

--- src/hexlet/23.2 Express/10 errors/errors.js
+++ src/hexlet/23.2 Express/10 errors/errors.js
@@ -1,6 +1,12 @@
 import Express from 'express';
 import morgan from 'morgan';
 
+const posts = [
+  { id: 1, title: 'Post 1', body: 'Post 1 content' },
+  { id: 2, title: 'Post 2', body: 'Post 2 content' },
+  { id: 3, title: 'Post 3', body: 'Post 3 content' },
+];
+
 const app = new Express;
 app.use(morgan('tiny'));
 
@@ -8,9 +14,23 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get('/posts/:id', (req, res, next) => {
+  const post = posts.find((post) => post.id.toString() === req.params.id);
+  if (post) {
+    res.send(post);
+  } else {
+    next(new Error(`User with id: ${req.params.id} does not exist`));
+  }
+});
+
 app.use((req, res) => {
   res.status('404')
   res.end('404');
-})
+});
+
+// src: http://expressjs.com/ru/guide/error-handling.html#%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0-%D0%BE%D1%88%D0%B8%D0%B1%D0%BE%D0%BA
+app.use((err, req, res, next) => {
+  res.end(`404 - ${err.message}`);
+});
 
 app.listen(8080, () => {});
